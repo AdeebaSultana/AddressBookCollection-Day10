269f1578f5d90141e2892887d858c2117751d1e7
AdeebaSultana <adizaan8892@gmail.com>
2022-04-19 20:55:41 +0530

    Added AddressBookMain Program

---
+++ src/com/ab/coll/Contact.java
@@ -0,0 +1,12 @@
+package com.ab.coll;
+
+public class Contact {
+    public String state;
+    public String zip;
+    public String phoneNumber;
+    public String email;
+    public String city;
+    public String address;
+    public String lastName;
+    public String firstName;
+}
